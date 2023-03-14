b689f28e7bc21269f59ec3714cc6207b3fe5f24e
JJJUhlar <jjuhlar@gmail.com>
2023-03-14 14:42:27 +0000

    refactored ui to give better user feedback when voting by using optimistic rendering

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -8,40 +8,38 @@ export const SingleReview = () => {
     const [fullReview, setFullReview] = useState({})
     const {review_id} = useParams()
     const [isLoading, setLoading] = useState(true)
+    const [visibleVotes, setVisibleVotes] = useState(0)
+    const [hasVoted, setHasVoted] = useState(false)
 
     useEffect(()=>{
         setLoading(false)
         fetchSingleReview(review_id)
             .then((res)=>{
                 setFullReview(res)
+                setVisibleVotes(fullReview.votes)
             })
     }, [review_id, fullReview])
 
 
     const handleVoteUp = () => {
+        setHasVoted(true)
+        setVisibleVotes(visibleVotes + 1)
         setFullReview({...fullReview}, fullReview.votes = fullReview.votes + 1)
-        console.log(fullReview.votes)
         sendReviewVote(review_id, +1)
-            .then((res)=>{
-                console.log(res)
-            })
-            .catch((err)=>{
-                console.log(err);
+            .then(()=>{
+                setVisibleVotes(fullReview.votes)
             })
     }
 
     const handleVoteDown = () => {
+        setHasVoted(true)
+        setVisibleVotes(visibleVotes - 1)
         setFullReview({...fullReview}, fullReview.votes = fullReview.votes - 1)
-        console.log(fullReview.votes)
         sendReviewVote(review_id, -1)
-            .then((res)=>{
-                console.log(res)
-            })
-            .catch((err)=>{
-                console.log(err);
+            .then(()=>{
+                setVisibleVotes(fullReview.votes)
             })
     }
-
 
     if (isLoading) {
         return (
@@ -54,14 +52,14 @@ export const SingleReview = () => {
             <h2>{fullReview.title}</h2>
             <h3>Reviewed by {fullReview.owner}</h3>
             <h4>{fullReview.category} Game, designed by {fullReview.designer}</h4>
-            <h5>Votes: {fullReview.votes}, Date: {fullReview.created_at}</h5>
+            <h5>Votes: {visibleVotes}, Date: {fullReview.created_at}</h5>
             <img src={fullReview.review_img_url} alt={fullReview.title}/>
             <p>
                 {fullReview.review_body}
             </p>
             <Link to='/'><button>Go back to all reviews</button></Link>
             <div className="votebuttons">
-                <button onClick={handleVoteUp}>Up Vote 👆</button> <button onClick={handleVoteDown}>Down Vote 👇</button>
+                <button onClick={handleVoteUp} disabled={hasVoted}>Up Vote 👆</button> <button onClick={handleVoteDown} disabled={hasVoted}>Down Vote 👇</button>
             </div>
             <ReviewComments />
         </section>
