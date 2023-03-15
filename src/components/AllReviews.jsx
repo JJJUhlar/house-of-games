1229d1c5794b5b758c354646323fb2109d61afe9
JJJUhlar <jjuhlar@gmail.com>
2023-03-15 09:06:07 +0000

    added a Filter component to take sort/order inputs

--- src/components/AllReviews.jsx
+++ src/components/AllReviews.jsx
@@ -2,11 +2,13 @@ import {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 import { fetchReviews } from "../utils/api"
 import { ReviewCard } from "./ReviewCard"
+import { Filter } from './Filter'
 
 export const AllReviews = () => {
     
     const [reviewItemsList, setReviewItemsList] = useState([])
     const [loading, setLoading] = useState(true)
+    const [filter, setFilter] = useState(null)
 
     useEffect(()=>{
         setLoading(false)
@@ -23,15 +25,18 @@ export const AllReviews = () => {
     }
 
     return (
-        <ul className='ReviewsList'>
-            {reviewItemsList.map((reviewItem)=>{
-                return (
-                    <li className="ReviewCard" key={reviewItem.review_id}>
-                        <ReviewCard reviewItem={reviewItem} />
-                        <Link to={`/reviews/${reviewItem.review_id}`}><button>Read More</button></Link>
-                    </li>                    
-                )
-            })}
-        </ul>
+        <>
+            <Filter filter={filter} setFilter={setFilter} />
+            <ul className='ReviewsList'>
+                {reviewItemsList.map((reviewItem)=>{
+                    return (
+                        <li className="ReviewCard" key={reviewItem.review_id}>
+                            <ReviewCard reviewItem={reviewItem} />
+                            <Link to={`/reviews/${reviewItem.review_id}`}><button>Read More</button></Link>
+                        </li>                    
+                    )
+                })}
+            </ul>
+        </>
     )
 }
