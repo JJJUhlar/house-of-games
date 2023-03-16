3305ef9a64fbaa871b2df6ba39bdd9c46f466556
JJJUhlar <jjuhlar@gmail.com>
2023-03-16 12:39:20 +0000

    fixed being able to post empty comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,7 +3,7 @@ import { UserContext } from "../contexts/user"
 import { deleteCommentById } from "../utils/api"
 
 export const CommentCard = ({comment}) => {
-
+    // console.log(comment.comment_id.toString())
     const {user} = useContext(UserContext)
     const [notOwnedByUser, setNotOwnedByUser] = useState(true)
     const [deleted, setDeleted] = useState(false)
@@ -26,13 +26,11 @@ export const CommentCard = ({comment}) => {
 
 
     return (
-        <>
-            <li key={comment.comment_id} hidden={deleted} className="commentCard">
+            <li key={(comment.comment_id).toString()} hidden={deleted} className="commentCard">
                 <p>"{comment.body}" - {comment.author}</p>
                 <p><em>at {comment.created_at}</em></p>
                 <p><em>Votes: {comment.votes}</em></p>
                 <button disabled={notOwnedByUser} hidden={notOwnedByUser} onClick={handleDeleteComment}>Delete Comment</button>
             </li>
-        </>
     )
 }
